07f798365c9d8d543d385faba06461a5d186b74b
Arindam Ghosh <61249444+NorahC03@users.noreply.github.com>
2022-10-28 23:07:07 +0530

    issue solved #14

---
+++ anagram.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    char A[] = "depp";
+    char B[] = "deap";
+    int as1 = sizeof(A) / sizeof(A[0]);
+    int as2 = sizeof(B) / sizeof(B[0]);
+    int h[128] = {0}, i, j;
+    bool flag = false;
+    if (as1 != as2)
+    {
+        cout << "This are not Anagrams" << endl;
+    }
+    else
+    {
+        for (i = 0; A[i] != '\0'; i++)
+        {
+            h[A[i]]++;
+        }
+        for (j = 0; B[j] != '\0'; j++)
+        {
+            h[B[j]]--;
+            if (h[B[j]] < 0)
+{
+                cout << "This are not Anagrams" << endl;
+                break;
+            }
+        }
+        if (B[j] == '\0')
+        {
+            cout << "This are Anagrams" << endl;
+        }
+    }
+    return 0;
+}
